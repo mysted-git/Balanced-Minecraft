7591f87280a902ceee055ea116c4252461132eef
HB0P <harrymilne2007@gmail.com>
2023-06-20 10:14:32 +0100

    iron golems cannot target persistent entities

--- src/main/java/com/balancedmc/mixins/village/M_IronGolemEntity.java
+++ src/main/java/com/balancedmc/mixins/village/M_IronGolemEntity.java
@@ -1,15 +1,18 @@
 package com.balancedmc.mixins.village;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.ai.pathing.SpiderNavigation;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.SpiderEntity;
 import net.minecraft.entity.passive.GolemEntity;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -37,6 +40,16 @@ public abstract class M_IronGolemEntity extends GolemEntity {
         return false;
     }
 
+    /**
+     * Iron golems cannot target persistent mobs
+     */
+    @Override
+    public void setTarget(@Nullable LivingEntity target) {
+        if (target instanceof MobEntity mob && mob.isPersistent()) {
+            return;
+        }
+        super.setTarget(target);
+    }
 
     /**
      * Iron golems can climb walls
