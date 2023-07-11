5755c5f245cca646ef219c6551ea3bd61446a50c
Mysted <kianbartley07@gmail.com>
2023-07-11 23:41:59 +0100

    Lingering potions crafted with phantom membrane Slow falling crafted with dragon's breath

--- src/main/java/com/balancedmc/mixins/potion/M_BrewingRecipeRegistry.java
+++ src/main/java/com/balancedmc/mixins/potion/M_BrewingRecipeRegistry.java
@@ -9,15 +9,18 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BrewingRecipeRegistry.class)
 public abstract class M_BrewingRecipeRegistry {
 
     @Shadow public static void registerPotionRecipe(Potion input, Item item, Potion output) {}
+    @Shadow public static void registerItemRecipe(Item input, Item ingredient, Item output) {}
 
     /**
      * Leaping is now brewed with a slimeball
+     * Slow falling is now brewed with phantom membrane
      */
     @Inject(
             method = "registerPotionRecipe(Lnet/minecraft/potion/Potion;Lnet/minecraft/item/Item;Lnet/minecraft/potion/Potion;)V",
@@ -25,8 +28,23 @@ public abstract class M_BrewingRecipeRegistry {
             cancellable = true
     )
     private static void injected(Potion input, Item item, Potion output, CallbackInfo ci) {
-        if (item == Items.RABBIT_FOOT && output == Potions.LEAPING) {
+        if (input == Potions.AWKWARD && item == Items.RABBIT_FOOT && output == Potions.LEAPING) {
             registerPotionRecipe(input, Items.SLIME_BALL, output);
+            ci.cancel();
+        } else if (input == Potions.AWKWARD && item == Items.PHANTOM_MEMBRANE && output == Potions.SLOW_FALLING) {
+            registerPotionRecipe(input, Items.DRAGON_BREATH, output);
+            ci.cancel();
+        }
+    }
+
+    @Inject(
+            method = "registerItemRecipe",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private static void registerItemRecipe(Item input, Item ingredient, Item output, CallbackInfo ci) {
+        if (input == Items.SPLASH_POTION && ingredient == Items.DRAGON_BREATH && output == Items.LINGERING_POTION) {
+            registerItemRecipe(input, Items.PHANTOM_MEMBRANE, output);
             ci.cancel();
         }
     }
